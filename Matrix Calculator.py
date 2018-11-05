5400be26387bc05f09cfe369ffccf73bd5a496d1
Disgustin <44754095+Disgustin@users.noreply.github.com>
2018-11-05 21:13:04 +0000

    Unused Code file added
    -Progression in the Create_Matrix function
    -Created file to hold unused code which I will need later

--- Matrix Calculator.py
+++ Matrix Calculator.py
@@ -1,22 +1,45 @@
 #import Matrix
 
-'''This will be used to test the program, this program will also be used to hold code that I don't want to use yet
+print("Welcome to that matrix calculator!\nThis program will you allow to create matrix..(Will add more features in the future) ") 
 
-while True:
-    try:
-        Column = int(input("Enter the amount of columns in your matrix: "))
-    except ValueError:
-        print("Error: Enter only whole numbers")
-    else:
-        break
+def main():
+    To_do = str(input("What would you like to do\nEnter 1 to: Create a matrix\nEnter 2 to: Find the determinates of a matrix\nEnter 3 to: Find the inverse of a matrix\nEnter 4 to: Perform calculations with matricies\n"))
 
-while True:
-    try:
-        Row = int(input("Enter the amount of rows in your matrix: "))
-    except ValueError:
-        print("Error: Enter only whole numbers")
-    else:
-        break
-'''
+    while To_do != '1' and To_do != '2' and To_do != '3' and To_do != '4':
+        To_do = str(input('Error: Invalid Input\nEnter 1 to: Create a matrix\nEnter 2 to: Find the determinates of a matrix\nEnter 3 to: Find the inverse of a matrix\nEnter 4 to: Perform calculations with matricies\n'))
+        print(type(To_do))
+    if To_do == '1':
+        while True:
+            try:
+                Column = int(input("Enter the amount of columns in your matrix: "))
+            except ValueError:
+                print("Error: Enter only whole numbers")
+            else:
+                break
 
-print("This is the test program to make sure the matrix library works externally.")
+        while True:
+            try:
+                Row = int(input("Enter the amount of rows in your matrix: "))
+            except ValueError:
+                print("Error: Enter only whole numbers")
+            else:
+                break
+        Matrix_Val = str(input("Enter the values of your matrix in order. Seperate each value with a comma\n"))
+        Matrix_Val = Matrix_Val.replace(' ','')
+        Mat_List_Check = Matrix_Val.replace(',','')
+        while len(Mat_List_Check) != (Column*Row):
+            Matrix_Val = str(input("Error: Invalid Matrix\nEnter the values of your matrix in order. Seperate each value with a comma\n"))
+            Matrix_Val = Matrix_Val.replace(' ','')
+            Mat_List_Check = Matrix_Val.replace(',','')
+            Mat_List = list(Mat_List_Check)
+        print(Mat_List_Check)
+        #Matrix.Create_Matrix(Column, Row, Mat_List)
+
+
+
+
+    
+
+
+main()
+#To use this function. Do Matrix.Create_Matrix(Column, Row, List of Data)
--- Matrix.py
+++ Matrix.py
@@ -9,40 +9,12 @@ Author: Mohamed Boudjillouli
 class Matrices:
     #Creating a matrix given that the user has given me some values.
 
-    def Create_Matrix():
-        while True:
-            try:
-                Column = int(input("Enter the amount of columns in your matrix: "))
-            except ValueError:
-                print("Error: Enter only whole numbers")
-            else:
-                break
-
-        while True:
-            try:
-                Row = int(input("Enter the amount of rows in your matrix: "))
-            except ValueError:
-                print("Error: Enter only whole numbers")
-            else:
-                break
-        Matrix_A = [[]]
-        C_Value_List = []
+    def Create_Matrix(Column, Row, Data):
         for i in range(Column):
-            C_Value = int(input("Enter the values for the columns of your matrix in order: "))
-            C_Value_List.append(C_Value)
-            Matrix_A.append(C_Value_List)
-            print(Matrix_A)
+            
             for o in range(Row):
-                R_Value = int(input("Enter the values for the rows for your matrix in order: "))
-                Matrix_A[len(Matrix_A)-1].append(R_Value)
-                print(Matrix_A)
-
-
-
 
         
-
-    Create_Matrix()
 
 
 
