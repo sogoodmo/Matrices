d2194bda5326d3448b1cc29bce0721bf20d66d9a
Disgustin <44754095+Disgustin@users.noreply.github.com>
2018-11-07 22:04:38 +0000

    Fix minor bugs

--- Matrix Calculator.py
+++ Matrix Calculator.py
@@ -4,6 +4,7 @@ import Matrix
 print("Welcome to that matrix calculator!\nThis program will you allow to create matrix..(Will add more features in the future) ") 
 
 def main():
+    Matrix_Num = 0
     To_do = str(input("\nWhat would you like to do\nEnter 1 to: Create a matrix\nEnter 2 to: Find the determinates of a matrix\nEnter 3 to: Find the inverse of a matrix\nEnter 4 to: Perform calculations with matricies\n"))
 
     #Spahgetti while loop to ensure they don't enter anything other than the avalible options
@@ -28,8 +29,21 @@ def main():
                 print("Error: Enter only whole numbers")
             else:
                 break
-        Matrix.Create_Matrix(Column, Row)
-        #Creating empty matrix to add my values to.
+        Matrix.Create_Matrix(Column, Row) #To use this function. Do Matrix.Create_Matrix(Column, Row, List of Data)
+        Matrix_Num += 1
+        Matrix.PlayAgain()
+    elif To_do == '2':
+        if Matrix_Num == 0:
+            To_do = 1
+        else:
+            Op = str(input("What operation would you like to perform\nEnter 1 to: For Addtion\nEnter 2 for: Subtraction\nEnter 3 for: Multiplication\nEnter 4 for: Division\n"))   
+            while Op != '1' and Op != '2' and Op != '3' and Op != '4':
+                Op = str(input('Error: Invalid Input\nEnter 1 to: For Addtion\nEnter 2 for: Subtraction\nEnter 3 for: Multiplication\nEnter 4 for: Division\n'))
+                print(type(Op))
+            if Op == '1':
+                print('DO SOME WORK!!!')
+
+
         
 main()
-#To use this function. Do Matrix.Create_Matrix(Column, Row, List of Data)
+
--- Matrix.py
+++ Matrix.py
@@ -1,3 +1,4 @@
+import time
 
 '''
 Hello. This is my first project where I will be attempting to create a library that can be used to
@@ -10,17 +11,42 @@ Author: Mohamed Boudjillouli
 #Creating a matrix given that the user has given me some values.
 def Create_Matrix(Column, Row):
     Main_Matrix = []
+
     #Creating row amount of empty 2d lists 
     for x in range(Row):
         Main_Matrix.append([])
+
     #Entering value of each matrix one at a time 
         for i in range(Column):
             Mat_Val = input("Enter the value of your matrix at row " + str(x+1) + " and at column " + str(i+1) + ": ")
             while not Mat_Val.isdigit() :
                   Mat_Val = input("Error: Invalid Matrix\nEnter the value of your matrix at row " + str(x+1) + " and at column " + str(i+1) + ": ")
             Mat_Val = int(Mat_Val)
-            Main_Matrix[x].append(Mat_Val)     
-        print(Main_Matrix[x])
+            Main_Matrix[x].append(Mat_Val)
+    for r in Main_Matrix: 
+        print(' '.join(str(x) for x in r))
 
-Create_Matrix(2,2)     
+def PlayAgain():
+    again = str(input("Do you want to play this quiz again? Y/N: ")).lower()
+    if again == "no" or again == "n":
+        print("Shutting Down...")
+        time.sleep(3)
+        quit()
+    if again == "yes" or again == "y":
+        main()
+    #If the input isnt "Yes, Y, No or N" ask the user to input a valid input.   
+    while (again != "y") or (again != "yes") or (again != "n") or (again != "no"):
+        again = str(input("Error: Invalid input\nDo you want to play this quiz again? Y/N: ")).lower()
+        if again == "no" or again == "n":
+            print("Shutting Down...")
+            time.sleep(3)
+            quit()
+        if again == "yes" or again == "y":
+            main()
+         
+       
+
+
+
+     
     
