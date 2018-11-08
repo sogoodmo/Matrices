be4d6472ed35bd695b590643f25f38a8df0a4da1
Disgustin <44754095+Disgustin@users.noreply.github.com>
2018-11-08 22:27:12 +0000

    Update comments

--- Matrix Calculator.py
+++ Matrix Calculator.py
@@ -1,11 +1,11 @@
 import Matrix
 
 #Introduction
-print("Welcome to that matrix calculator!\nThis program will you allow to create matrix..(Will add more features in the future) ") 
+print('Welcome to that matrix calculator!\nThis program will you allow to create matrix..(Will add more features in the future) ') 
 
 def main():
     Matrix_Num = 0
-    To_do = str(input("\nWhat would you like to do\nEnter 1 to: Create a matrix\nEnter 2 to: Find the determinates of a matrix\nEnter 3 to: Find the inverse of a matrix\nEnter 4 to: Perform calculations with matricies\n"))
+    To_do = str(input('\nWhat would you like to do\nEnter 1 to: Create a matrix\nEnter 2 to: Find the determinates of a matrix\nEnter 3 to: Find the inverse of a matrix\nEnter 4 to: Perform calculations with matricies\n'))
 
     #Spahgetti while loop to ensure they don't enter anything other than the avalible options
     while To_do != '1' and To_do != '2' and To_do != '3' and To_do != '4':
@@ -16,16 +16,16 @@ def main():
         #Can't have a matrix with 2.3 rows
         while True:
             try:
-                Column = int(input("Enter the amount of columns in your matrix: "))
+                Column = int(input('Enter the amount of columns in your matrix: '))
             except ValueError:
-                print("Error: Enter only whole numbers")
+                print('Error: Enter only whole numbers')
             else:
                 break
         while True:
             try:
-                Row = int(input("Enter the amount of rows in your matrix: "))
+                Row = int(input('Enter the amount of rows in your matrix: '))
             except ValueError:
-                print("Error: Enter only whole numbers")
+                print('Error: Enter only whole numbers')
             else:
                 break
         Matrix.Create_Matrix(Column, Row)
@@ -33,12 +33,12 @@ def main():
 
         #Matrix.PlayAgain()
 
-    #Next function. Operations with matricies.    
+    #Next function. Performing opertations with matricies such as add, sub, div, multi.    
     elif To_do == '2':
         if Matrix_Num == 0:
             To_do = 1
         else:
-            Op = str(input("What operation would you like to perform\nEnter 1 to: For Addtion\nEnter 2 for: Subtraction\nEnter 3 for: Multiplication\nEnter 4 for: Division\n"))   
+            Op = str(input('What operation would you like to perform\nEnter 1 to: For Addtion\nEnter 2 for: Subtraction\nEnter 3 for: Multiplication\nEnter 4 for: Division\n'))   
             while Op != '1' and Op != '2' and Op != '3' and Op != '4':
                 Op = str(input('Error: Invalid Input\nEnter 1 to: For Addtion\nEnter 2 for: Subtraction\nEnter 3 for: Multiplication\nEnter 4 for: Division\n'))
                 print(type(Op))
--- Matrix.py
+++ Matrix.py
@@ -18,33 +18,33 @@ def Create_Matrix(Column, Row): #To use this function. Do Matrix.Create_Matrix(C
 
     #Entering value of each matrix one at a time, repeat this Row amount of times
         for i in range(Column):
-            Mat_Val = input("Enter the value of your matrix at row " + str(x+1) + " and at column " + str(i+1) + ": ")
+            Mat_Val = input('Enter the value of your matrix at row ' + str(x+1) + ' and at column ' + str(i+1) + ': ')
             while not Mat_Val.isdigit() :
-                  Mat_Val = input("Error: Invalid Matrix\nEnter the value of your matrix at row " + str(x+1) + " and at column " + str(i+1) + ": ")
+                  Mat_Val = input('Error: Invalid Matrix\nEnter the value of your matrix at row ' + str(x+1) + ' and at column ' + str(i+1) + ': ')
             Mat_Val = int(Mat_Val)
             Main_Matrix[x].append(Mat_Val)
     
-    #for each value in the final list, join it in a string with a comma.
+    #For each value in the final list, join it in a string with a comma.
     for r in Main_Matrix: 
        print(', '.join(str(x) for x in r))
 
 
 '''def PlayAgain():
-    again = str(input("Do you want to play this quiz again? Y/N: ")).lower()
-    if again == "no" or again == "n":
-        print("Shutting Down...")
+    again = str(input('Do you want to play this quiz again? Y/N: ')).lower()
+    if again == 'no' or again == 'n':
+        print('Shutting Down...')
         time.sleep(3)
         quit()
-    if again == "yes" or again == "y":
+    if again == 'yes' or again == 'y':
         main()
-    #If the input isnt "Yes, Y, No or N" ask the user to input a valid input.   
-    while (again != "y") or (again != "yes") or (again != "n") or (again != "no"):
-        again = str(input("Error: Invalid input\nDo you want to play this quiz again? Y/N: ")).lower()
-        if again == "no" or again == "n":
-            print("Shutting Down...")
+    #If the input isnt 'Yes, Y, No or N' ask the user to input a valid input.   
+    while (again != 'y') or (again != 'yes') or (again != 'n') or (again != 'no'):
+        again = str(input('Error: Invalid input\nDo you want to play this quiz again? Y/N: ')).lower()
+        if again == 'no' or again == 'n':
+            print('Shutting Down...')
             time.sleep(3)
             quit()
-        if again == "yes" or again == "y":
+        if again == 'yes' or again == 'y':
             main()
          '''
        
