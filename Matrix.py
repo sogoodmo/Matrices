51766d17f7e202758d1e80b120ebed4d946192c6
Disgustin <44754095+Disgustin@users.noreply.github.com>
2018-11-14 22:54:48 +0000

    Add matrix multiplication

--- Matrix.py
+++ Matrix.py
@@ -10,6 +10,7 @@ def create_matrix(row,column,list_of_values):
 
     for k in result_matrix: 
        print(', '.join(str(i) for i in k))
+    print('\n')
 
     return result_matrix
 
@@ -28,7 +29,6 @@ def add_matrix(matrix_1,matrix_2):
 
     return addition_result_matrix
 
-print(add_matrix(matrix_1,matrix_2))
 
 #Function 3:
 #Subtrasction with matricies returning 
@@ -41,7 +41,7 @@ def sub_matrix(matrix_1,matrix_2):
             subtraction_result_matrix[i].append(matrix_1[i][j]-matrix_2[i][j])
 
     return subtraction_result_matrix
-print(sub_matrix(matrix_1,matrix_2))
+
 
 #Function 4:
 #Sclar multiplication of a matrix
@@ -54,23 +54,28 @@ def scalar_multi(matrix_1,scale_factor):
             scalar_multi_result_matrix[i].append((matrix_1[i][j])*4)
 
     return scalar_multi_result_matrix
-print(scalar_multi(matrix_1,4))
+
 
 #Function 5:
 #Multiplying matricies together
 def matrix_multiply(matrix_1,matrix_2):
     multi_result_matrix = []
-    val_TO_ADD = 0
+    
+    
 
     matrix_1_row = len(matrix_1)
-    matrix_1_column = len(matrix_1[0])
-    matrix_2_row = len(matrix_2)
     matrix_2_column = len(matrix_2[0])
 
-    for i in range(matrix_1_row*matrix_2_column):
+    for i in range(matrix_1_row):
         for j in range(matrix_2_column):
-            multi_result_matrix.append(int(str(matrix_1[j][i]))*int(str(matrix_2[j][i]))) #broken 
+            element_value = 0
+            for l in range(matrix_2_column):
+                element_value += (int(str(matrix_1[i][l]))*int(str(matrix_2[l][j])))
+            multi_result_matrix.append(element_value)
     print(multi_result_matrix)
+
+    
+    
 matrix_multiply(matrix_1,matrix_2) 
         
 
